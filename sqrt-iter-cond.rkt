ccdd49df3fa615803e458996f5ab3d6eef0e2c50
Searge <ashikcygan@gmail.com>
2018-06-20 22:32:31 +0300

    Examples from first chapters

---
+++ sqrt-iter-cond.rkt
@@ -0,0 +1,12 @@
+;; Новий контейнер з операцій по знаходженню кв. кореню
+
+(define (sqrt x)
+  (define (good-enough? guess) 
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
