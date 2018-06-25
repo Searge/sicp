3725ff9eb640de97ae6297bfb73222583b28f296
Searge <ashikcygan@me.com>
2018-06-25 14:18:19 +0300

    Exersize 1.3

--- samples/min-max-sum.rkt
+++ samples/min-max-sum.rkt
@@ -1,12 +1,10 @@
 #lang racket
 
-(define (square x) (* x x))
-
-(define (max a b)
-  (if (> a b) a b))
-
-(define (min a b)
-  (if (< a b) a b))
-
 (define (largest-sums x y z)
-  (+ (square (max x y)) (square (max (min x y) z))))
+  (define (square x) (* x x))
+  (define (max a b)
+    (if (> a b) a b))
+  (define (min a b)
+    (if (< a b) a b))
+  (+ (square (max x y)) (square (max (min x y) z)))
+  )
