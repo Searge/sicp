f21921d3637c78865a15034cc04d2c0890770b69
Searge <ashikcygan@me.com>
2018-06-26 16:38:12 +0300

    Update exercises

--- samples/sqrt-iter-cond.rkt
+++ samples/sqrt-iter-cond.rkt
@@ -1,12 +1,10 @@
+#lang racket
 ;; Новий контейнер з операцій по знаходженню кв. кореню
 
 (define (sqrt x)
-  (define (good-enough? guess) 
-    (< (abs (- (square guess) x)) 0.001))
-  (define (improve guess)
-    (average guess (/ x guess)))
-  (define (sqrt-iter guess)
-    (if (good-enough? guess)
-        guess
-        (sqrt-iter (improve guess))))
+  (define (square x) (* x x))
+  (define (average x y) (/ (+ x y) 2))
+  (define (improve y) (average y (/ x y)))
+  (define (good-enough? y) (< (abs (- (square y) x)) 0.001))
+  (define (sqrt-iter y) (if (good-enough? y) y (sqrt-iter (improve y))))
   (sqrt-iter 1.0))
