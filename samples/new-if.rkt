62587b52c62fcf8348db190adf514e76e0d8cd6f
Searge <ashikcygan@me.com>
2018-06-26 17:50:35 +0300

    Update exercises

---
+++ samples/new-if.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(define (square x) (* x x))
+(define (sqrt x) (sqrt-iter 1.0 x))
+(define (average x y) (/ (+ x y) 2))
+(define (improve guess x) (average guess (/ x guess)))
+(define (good-enough? guess x) (< (abs (- (square guess) x)) 0.001))
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x) guess (sqrt-iter (improve guess x) x)))
--- samples/sqrt-iter.rkt
+++ samples/sqrt-iter.rkt
@@ -1,17 +1,14 @@
 #lang racket
 
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x) guess (sqrt-iter (improve guess x) x)))
-
-(define (improve guess x) (average guess (/ x guess)))
-
-(define (average x y) (/ (+ x y) 2))
-
 (define (square x) (* x x))
-
+(define (sqrt x) (sqrt-iter 1.0 x))
+(define (average x y) (/ (+ x y) 2))
+(define (improve guess x) (average guess (/ x guess)))
 (define (good-enough? guess x) (< (abs (- (square guess) x)) 0.001))
 ;; abs — процедура racket'a
-(define (sqrt x) (sqrt-iter 1.0 x))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x) guess (sqrt-iter (improve guess x) x)))
 
 ;; > (sqrt 9)
 ;; 3.00009155413138
