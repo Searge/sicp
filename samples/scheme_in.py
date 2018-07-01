4d51506347f4ca6717d1f229d55b113888f8217b
Searge <ashikcygan@gmail.com>
2018-07-01 17:59:39 +0300

    Create scheme_in.py

---
+++ samples/scheme_in.py
@@ -0,0 +1,8 @@
+def sum_squares(x, y):
+    return add(square(x), square(y))
+def print_square(x):
+    print(square(x))
+def percent_difference(x, y):
+    difference = abs(x-y)
+    return 100 * difference / x
+    
